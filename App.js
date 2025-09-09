7cc72a14eadb962242b7c57decc2d10d9aa1e7ba
Neha Kumari <neha.kumari80923@gmail.com>
2025-09-09 20:08:36 +0530

    add files

--- App.js
+++ App.js
@@ -1,62 +1,49 @@
-//  /**
-//   * 
-//   * <div id="parent">
-//   *  <div id="child">
-//   * <h1>I'm h1 tag</h1>
-//   * <h2>I'm h1 tag</h2>
-//   * </div>
-//   *  <div id="child2">
-//   * <h1>I'm h1 tag</h1>
-//   * <h2>I'm h1 tag</h2>
-//   * </div>
-//   * 
-//   * </div>
-//   * 
-//   * ReactElement(object) = > HTML(browser understands)
-//   */ 
- 
-//  //nested components
- 
-// //JSX (javascript extension of javascript)
-//  //react is  only written using jsx.jsx is make life easier to write the code in react.
-
-//     root.render (parent);//  this render method is basically  converting  this  object to  and  the <h1> tag inside the root div of the HTML page.
-//     //render() method basically React object ko real DOM <h1> tag me convert karke root div ke andar insert karta hai.
-//     // This is how this code works behind the scenes. // browser pr kaam krta hai.
-
-
-
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" }, [
-    React.createElement(
-      "div",
-      { id: "child", key: "child1" }, [   //  key added 
-        React.createElement("h1", { key: "h1-1" }, "hii dimpal"),
-        React.createElement("h2", { key: "h2-1" }, "I'm an h2 tag"),
-      ]
-    ),
-    React.createElement(
-      "div",
-      { id: "child2", key: "child2" }, [   // key added 
-        React.createElement("h1", { key: "h1-2" }, "I'm an h1 tag"),
-        React.createElement("h2", { key: "h2-2" }, "I'm an h2 tag"),
-      ]
-    ),
-  ]
-);
+/**
+ * Header
+ * it will have a logo component and a heading component.
+ * Nav Item
+ * Body
+ *  - Search
+ *  - RestaurantContainer
+ *  - RestaurantCard
+ * Footer
+ *  - copyright
+ *  - Address
+ *  - Contact
+ */
 
-console.log(parent);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://static.vecteezy.com/system/resources/previews/011/468/885/non_2x/food-logo-spoon-fork-icon-illustration-symbol-for-fast-delivery-app-restaurant-template-free-vector.jpg"
+        />
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return(
+<div className="app">
+  <Header/>
+  </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
 
-
-
-
-
-
+root.render(<AppLayout />);
